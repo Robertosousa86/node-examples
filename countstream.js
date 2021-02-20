45a83473a1be0b12d5cf88080008dca9f3e92a81
Roberto Sousa <robertosousa186@gmail.com>
2021-02-19 23:39:53 -0200

    first example: contando palavras

---
+++ countstream.js
@@ -0,0 +1,29 @@
+var Writable = require("stream").Writable;
+var util = require("util");
+
+module.exports = CountStream;
+// herda de writable stream
+util.inherits(CountStream, Writable); 
+
+//
+function CountStream(matchText, options) {
+  Writable.call(this, options);
+  this.count = 0;
+  // cria um objeto de expressão regular que corresponda globalmente e ignore maiúsculas
+  this.matcher = new RegExp(matchText, "ig"); 
+}
+
+CountStream.prototype._write = function (chunck, encoding, cb) {
+  // converter o bloco atual de entradas em strings e as utiliza para contar igualdades
+  var matches = chunck.toString().match(this.matcher); 
+
+  if (matches) {
+    this.count += matches.length;
+  }
+  cb();
+};
+
+CountStream.prototype.end = function () {
+  // quando o total de linhas acaba, posta o total de correspondências(igualdades)
+  this.emit("total", this.count); 
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+// faz o require do modulo countstream
+var CountStream = require("./countstream");
+// instancia a classe CountStream que conta o texto 'book'
+var countStream = new CountStream("São Paulo");
+var http = require("http");
+// faz a requisição no site
+http.get("http://www.saopaulofc.net/", function (res) {
+  // faz o pipe dos dados do site para coutStream, contando o texto
+  res.pipe(countStream);
+});
+
+// resultado de 4 correspondências
+countStream.on("total", function (count) {
+  console.log("Total matches", count);
+});
